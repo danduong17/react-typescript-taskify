a20aedbc16fe7244d9f848fe281212efcd57fc0e
danduong17 <16079@student.vgu.edu.vn>
2023-05-23 16:59:35 +0700

    Setup React, delete some unnecessary files & create Input Field to input task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InputField from './components/InputField';
 
-function App() {
+// As App is a functional component so declare App as type called FC of React
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField />
     </div>
   );
 }
