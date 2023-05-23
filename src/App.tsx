7c5d480032c2a2b529485ccdb3f530b1955c781b
danduong17 <16079@student.vgu.edu.vn>
2023-05-23 19:46:09 +0700

    Add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import './App.css';
-import InputField from './components/InputField';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
 
 // As App is a functional component so declare App as type called FC of React
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  /* Because of using TypeScript, everything in TypeScript must have a type, even a state.
+  So I would give this state a type string because the task when inputting is just a string */
+
+  // console.log(todo); 
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react";
 import "./styles.css";
 
-const InputField = () => {
-  return (
-    <form className='input'>
-        <input type='text' placeholder='Enter a task' className='input-box'/> 
-        <button className='input-submit' type='submit'>Go</button>
-    </form>
-  )
+/* Although has already added todo & setTodo to this component, still some have errors because:
+todo implicitly has an 'any' type. Solution: add interface to declare types for todo & setTodo */
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  // setTodo is a function, so to know what type of setTodo is go back to App.tsx & hover setTodo, it'll point out
 }
 
-export default InputField
+// There's another way to define Props bc this is a functional component:
+// const InputField:React.FC<Props> = ...
+const InputField = ({ todo, setTodo }: Props) => {
+  return (
+    <form className="input">
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input-box"
+      />
+      <button className="input-submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
